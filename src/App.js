15a442f734fe0fa8edd668294f7d55bd2f884401
mohamed <mibrahemsaad@gmail.com>
2024-03-08 17:16:07 +0200

    added the project

---
+++ src/App.js
@@ -0,0 +1,36 @@
+import React from "react";
+import "./App.css";
+import Main from "./Components/Main/Main";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import FilteredProducts from "./Components/FiltredProducts/FilteredProducts";
+import SingleProduct from "./Components/FiltredProducts/SingleProduct";
+import Register from "./Components/Login/Register";
+import { useSelector } from "react-redux";
+function App() {
+  const user = useSelector((state) => state.user.user);
+  const { authUser } = user;
+
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route
+            path="/"
+            element={authUser ? <Main></Main> : <Register></Register>}
+          ></Route>
+
+          <Route
+            path="/filteredProducts/:type"
+            element={<FilteredProducts></FilteredProducts>}
+          ></Route>
+          <Route
+            path="/filteredProducts/:type/:id"
+            element={<SingleProduct></SingleProduct>}
+          ></Route>
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/api/axios.js
@@ -0,0 +1,5 @@
+import axios from 'axios';
+
+export default axios.create({
+    baseURL: 'mongodb+srv://mohamedIbrahim:hTg2cDAFW05itBG0@cluster0.3morka7.mongodb.net/'
+});
